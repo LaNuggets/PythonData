7d250fca65f54b212e1d8cb666e188d4e5df983e
LaNuggets <aurelien.dugast44@gmail.com>
2025-03-18 14:30:52 +0100

    add report html

--- read.py
+++ read.py
@@ -1,11 +1,23 @@
 import pandas as pd
 
 df = pd.read_csv("train.csv")
+
+
 # df['isMale'] = df["Sex"].apply(lambda x: 1 if x == 'male' else 0) # add column isMale 1 or 0
-# print(df.groupby('Sex')["Survived"].mean())
+
+# df=df[df["Survived"]==1]
+
+# print(df.groupby('Pclass')["Fare"].mean())
+
 
 # df['Age_group'] = df['Age'].apply(lambda x: '-10' if x <= 10 else '+10') # add columns and get the mean of people who survived under 10 year old
 # print(df.groupby('Age_group')['Survived'].mean())
 
 
 # print(df["Sex"].eq("male").mean()) # get male percentage
+
+
+# from ydata_profiling import ProfileReport
+ 
+# profile = ProfileReport(df, title="Profiling Report")
+# profile.to_file("report.html")
