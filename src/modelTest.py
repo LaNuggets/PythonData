e3de8aec57a8d0e34660169d050fb2063bac94ba
daurelie <aurelien.dugast@ynov.com>
2025-04-01 15:58:28 +0200

    add model and preprocessor

---
+++ src/modelTest.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+# from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+
+df = pd.read_csv("train.csv") # récupération du csv
+
+df['isMale'] = df["Sex"].apply(lambda x: 1 if x == 'male' else 0) # création d'une nouvelle colonne pour savoir si le passager est un homme ou une femme mais en donné binaire
+df['Age'] = df['Age'].fillna(df['Age'].mean()) # remplir les ages manquant par l'age moyen
+
+df = df.drop('Name', axis=1) # suppression de la table "Name"
+df = df.drop('Sex', axis=1) # suppression de la table "Sex"
+df = df.drop('Ticket', axis=1) # suppression de la table "Ticket"
+df = df.drop('Cabin', axis=1) # suppression de la table "Cabin"
+df = df.drop('Embarked', axis=1) # suppression de la table "Embarked"
+
+X = df.drop('Survived', axis=1) # récupération de la table des survivants
+y = df['Survived']
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+test_size=0.2,
+random_state=42)
+
+model = LogisticRegression(max_iter=1000) # création d'une instance de LogisticRegression
+# model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train) # entrainement du model
+y_pred = model.predict(X_test) # résultat des prédictions
+
+acc = accuracy_score(y_test, y_pred) # calcule de l'accuracy
+f1 = f1_score(y_test, y_pred, average='binary')
+
+print(acc, f1)
+cm = confusion_matrix(y_test, y_pred) # création de matrice de confusion
+print(cm)
---
+++ src/preprocessor.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+class TitanicPreprocessor:
+    def __init__(self):
+        self.df = pd.read_csv("../data/train.csv")
+
+    def fit(self, df):
+        self.age_moyen=df['Age'].mean()
+    
+    def transform(self, df):
+        df['isMale'] = df["Sex"].apply(lambda x: 1 if x == 'male' else 0) # création d'une nouvelle colonne pour savoir si le passager est un homme ou une femme mais en donné binaire
+        
+        df['Age'] = df['Age'].fillna(self.age_moyen) # remplir les ages manquant par l'age moyen
+        
+        df = df.drop('Name', axis=1) # suppression de la table "Name"
+        df = df.drop('Sex', axis=1) # suppression de la table "Sex"
+        df = df.drop('Ticket', axis=1) # suppression de la table "Ticket"
+        df = df.drop('Cabin', axis=1) # suppression de la table "Cabin"
+        df = df.drop('Embarked', axis=1) # suppression de la table "Embarked"
+        
+        X = df.drop('Survived', axis=1) # récupération de la table des survivants
+        y = df['Survived']
+        return X, y
+
+    def fit_transform(self, df):
+        self.fit()
+        X, y = self.transform(df);
+        return X, y
