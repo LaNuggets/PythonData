e20634b7e56cd499afa1ae70296d720da5bbabe5
daurelie <aurelien.dugast@ynov.com>
2025-04-08 13:47:19 +0200

    remove comment

--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
-from src.preprocessor import TitanicPreprocessor
-from src.model import TitanicModel
+from preprocessor import TitanicPreprocessor
+from TitanicModel import TitanicModel
 import pandas as pd
 
 def main():
-    # Chargement des données
     df = pd.read_csv("data/train.csv")
-    # Prétraitement
+
     prep = TitanicPreprocessor()
     X, y = prep.fit_transform(df)
-    # Entraînement du modèle
+
     model = TitanicModel()
     model.train(X, y)
-    # Prédiction sur les mêmes données pour l'exemple
+
     y_pred = model.predict(X)
     print("Prédictions :", y_pred[:5])
     
